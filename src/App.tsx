10b909cb07249da28aaaff422da8d8b41922417d
haidyhere <haidyhere@gmail.com>
2025-05-09 23:45:26 -0700

    update labs

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
+import Labs from "./Labs";
 
 function App() {
   //const [count, setCount] = useState(0)
 
-  return <h1>Welcome to Web Dev</h1>;
+  return (
+    <div>
+      <Labs />
+    </div>
+  )
     
 }
 
