9242e512e3687ae64bce952d0b979f04c04e6f62
haidyhere <haidyhere@gmail.com>
2025-05-11 01:18:01 -0700

    update to 1.3

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
 import Labs from "./Labs";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 
-function App() {
-  //const [count, setCount] = useState(0)
-
+export default function App() {
+  
   return (
+    <HashRouter>
     <div>
+      <Routes>
+        <Route path="/" element={<Navigate to="Labs" />} />
+        <Route path="/Labs/*" element={<Labs />} />
+      </Routes>
       <Labs />
     </div>
-  )
-    
-}
-
-export default App
+    </HashRouter>
+  );}
---
+++ src/Labs/index.tsx
@@ -0,0 +1,24 @@
+import Lab1 from "./Lab1";
+import {Route, Routes, Navigate} from "react-router-dom";
+import TOC from "./TOC";
+import Lab2 from "./Lab2";
+import Lab3 from "./Lab3";
+export default function Labs() {
+    return (
+        <div id="wd-labs">
+            <h1>Labs</h1>
+            <p>Hongjing Ren</p>
+            <p>Section: Summer 1, 2025</p>
+            <TOC />
+            <Routes>
+                <Route path="/" element={<Navigate to="Lab1" />} />
+                <Route path="Lab1" element={<Lab1 />} />
+                <Route path="Lab2/*" element={<Lab2 />} />
+                <Route path="Lab3/*" element={<Lab3 />} />
+            </Routes>
+            <Lab1 />
+            <Lab2 />
+            <Lab3 />
+        </div>
+    );
+}
