b73e1eb5546682edb77f5ca5281866d94a8be352
haidy <ren.hong@northeastern.edu>
2025-06-15 01:48:27 -0700

    name

--- src/Kambaz/Account/Users.tsx
+++ src/Kambaz/Account/Users.tsx
@@ -54,7 +54,9 @@ export default function Users() {
         Users
       </button>
       <h3>Users</h3> 
-      <FormControl onChange={(e) => filterUsersByName(e.target.value)} placeholder="Search people" 
+      <FormControl 
+        value={name}
+      onChange={(e) => filterUsersByName(e.target.value)} placeholder="Search people" 
         className="float-start w-25 me-2 wd-filter-by-name" />
       <select value={role} onChange={(e) =>filterUsersByRole(e.target.value)} 
         className="form-select float-start w-25 wd-select-role" > 
